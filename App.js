3ca577a94bd557f636939a8043db7c18e15a2a17
lemon <19805815940@163.com>
2019-04-14 18:32:59 +0800

    🚀 Initializing project.

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AppContainer from './app/router/index';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <AppContainer onNavigationStateChange={this.handleNavigationChange} />
+      </View>
+    );
+  }
+}
