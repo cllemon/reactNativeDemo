647cc5a38e1d43ad7e0c7e12cdbca1a998e4f879
lemon <19805815940@163.com>
2019-04-15 23:26:11 +0800

    🙌 New package lower back switch component.

--- app/package/switch/index.js
+++ app/package/switch/index.js
@@ -5,16 +5,61 @@ import Package from '../../widget/package';
 
 const COMPONENT_LABEL = '跨平台通用的“开关”';
 const COMPONENT_VALUE = 'Switch';
+const SWITCH_LIST = [
+  {
+    type: 'oneValue',
+    disabled: true
+  },
+  {
+    type: 'secondValue',
+    thumbColor: '#fff',
+    trackColor: { true: '#67C23A', false: '#dcdfe6' }
+  },
+  {
+    type: 'thirdValue',
+    thumbColor: '#fff',
+    trackColor: { true: '#409eff', false: '#dcdfe6' }
+  },
+  {
+    type: 'fourValue'
+  }
+];
 
 class SwitchPackage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      thirdValue: true
+    };
   }
 
   previewDemoOne = () => {
-    return <View />;
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        {SWITCH_LIST.map((item, index) => {
+          return (
+            <Switch
+              key={index}
+              style={{ padding: 20 }}
+              value={
+                this.state[
+                  item.type
+                ] /** 表示此开关是否打开。默认为false（关闭状态 */
+              }
+              disabled={item.disabled}
+              trackColor={item.trackColor /**开启状态时的背景颜色。 */}
+              thumbColor={
+                item.thumbColor /**开关上圆形按钮的背景颜色。在iOS上设置此颜色会丢失按钮的投影。 */
+              }
+              onValueChange={value => {
+                this.setState({ [item.type]: value });
+              }}
+            />
+          );
+        })}
+      </View>
+    );
   };
 
   render() {
