c1216009733337df280cd6b65552d58dd6c08746
lemon <19805815940@163.com>
2019-04-15 23:26:51 +0800

    🙌 New lower back slider component of package.

--- app/package/slider/index.js
+++ app/package/slider/index.js
@@ -1,20 +1,92 @@
 import React, { Component } from 'react';
-import { View, Slider } from 'react-native';
+import { View, Slider, Text } from 'react-native';
 import Card from '../../widget/card';
 import Package from '../../widget/package';
 
 const COMPONENT_LABEL = '范围值选择器';
 const COMPONENT_VALUE = 'Slider';
+const SLIDER_LIST = [
+  {
+    type: 'oneValue',
+    style: {} // 默认
+  },
+  {
+    type: 'secondValue',
+    value: 30,
+    step: 1,
+    maximumValue: 100,
+    minimumValue: 0
+  },
+  {
+    type: 'thirdValue',
+    step: 10,
+    maximumValue: 100,
+    minimumValue: 0
+  },
+  {
+    type: 'fourValue',
+    value: 30,
+    disabled: true,
+    maximumValue: 100,
+    minimumValue: 0
+  },
+  {
+    type: 'fiveValue',
+    maximumValue: 100,
+    minimumValue: 10,
+    step: 1,
+    minimumTrackTintColor: '#F56C6C',
+    maximumTrackTintColor: '#67C23A'
+  }
+];
 
 class SliderPackage extends Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
 
+  onValueChange = (value, type) => {
+    if (this.state[type] !== value) {
+      this.setState({ [type]: value });
+    }
+  };
+
+  formatter = (val = 0) => {
+    return val.toFixed(2);
+  };
+
   previewDemoOne = () => {
-    return <View />;
+    return (
+      <View>
+        {SLIDER_LIST.map((item, index) => {
+          return (
+            <View style={{ flexDirection: 'row' }} key={index}>
+              <Slider
+                style={[{ width: 300, height: 60 }, item.style]}
+                step={item.step}
+                value={item.value}
+                disabled={item.disabled}
+                maximumValue={item.maximumValue}
+                minimumValue={item.minimumValue}
+                minimumTrackTintColor={item.minimumTrackTintColor}
+                maximumTrackTintColor={item.maximumTrackTintColor}
+                onValueChange={value => {
+                  this.onValueChange(value, item.type);
+                }}
+                thumbImage={item.thumbImage}
+                trackImage={item.trackImage}
+                minimumTrackImage={item.minimumTrackImage}
+                maximumTrackImage={item.maximumTrackImage}
+              />
+              <Text style={{ paddingTop: 18 }}>
+                {this.formatter(this.state[item.type] || item.value)}
+              </Text>
+            </View>
+          );
+        })}
+      </View>
+    );
   };
 
   render() {
@@ -25,7 +97,7 @@ class SliderPackage extends Component {
         navigation={this.props.navigation}
       >
         {/** demo - 1 */}
-        <Card html={[COMPONENT_VALUE, 'FIRST']} codeHeight={2044}>
+        <Card html={[COMPONENT_VALUE, 'FIRST']} codeHeight={444}>
           {this.previewDemoOne()}
         </Card>
       </Package>
